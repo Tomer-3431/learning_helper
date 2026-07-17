dc258a66b40f89866e51de6df74e39b120ee1603
Hatulim <tomer10.katz@gmail.com>
2026-07-17 17:16:11 +0300

    move MainApp to home screen

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -6,3 +6,18 @@ final TextStyle h1 = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
+final AppBarTheme appBarTheme = AppBarTheme(
+  backgroundColor: Colors.green,
+  elevation: 32.0,
+  shadowColor: Colors.green.withValues(alpha: 0.3),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.vertical(bottom: Radius.circular(8)),
+  ),
+  centerTitle: true,
+  titleTextStyle: const TextStyle(
+    fontSize: 26,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    decorationThickness: 2,
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learning_helper/constants/ui_constants.dart';
+import 'package:learning_helper/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: Colors.green.shade900,
+        cardColor: Colors.teal.shade100,
+        appBarTheme: appBarTheme,
+        brightness: Brightness.dark,
       ),
+      home: HomeScreen(),
     );
   }
 }
