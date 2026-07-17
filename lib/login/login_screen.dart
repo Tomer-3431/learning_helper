819530fd18ec55ebe43f7c72a5821ab788645b9e
Hatulim <tomer10.katz@gmail.com>
2026-07-17 17:42:56 +0300

    fix ui issues

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -16,17 +16,29 @@ class _LoginScreenState extends State<LoginScreen> {
         padding: EdgeInsets.all(32),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
+          spacing: 10,
           children: [
+            Spacer(),
             Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 _buildLogin(),
-                Divider(),
-                _buildSignup()
+                Container(
+                  height: 500,
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      vertical: BorderSide(width: 2, color: Colors.black),
+                    ),
+                  ),
+                ),
+                _buildSignup(),
               ],
             ),
+            Spacer(),
+            Spacer(),
             Divider(),
             _buildGoogleLogin(),
+            Spacer(),
           ],
         ),
       ),
@@ -34,37 +46,51 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _buildLogin() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 20,
-        horizontal: 10
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Login'
-          ),
-        ],
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 20,
+          children: [
+            Text('Login', style: TextStyle(fontSize: 18)),
+            Text(
+              'UserName',
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            Text('Email', style: TextStyle(decoration: TextDecoration.underline)),
+            Text(
+              'Password',
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildSignup() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 20,
-        horizontal: 10
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Sign Up'
-          ),
-        ],
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 20,
+          children: [
+            Text('Sign Up', style: TextStyle(fontSize: 18)),
+            Text(
+              'UserName',
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            Text('Email', style: TextStyle(decoration: TextDecoration.underline)),
+            Text(
+              'Password',
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -73,16 +99,20 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.baseline,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         spacing: 20,
         children: [
           Text('Login With Google'),
           Container(
+            width: 100,
+            height: 50,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
               border: Border.all(color: Theme.of(context).primaryColor),
             ),
-            child: Center(child: Text('PLACE HOLDER')),
+            child: Center(
+              child: Text('PLACE HOLDER', textAlign: TextAlign.center),
+            ),
           ),
         ],
       ),
