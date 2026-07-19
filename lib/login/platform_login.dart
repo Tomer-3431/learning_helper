6e409f9545d1fd9256a2f8da4427a735cf0a343c
Hatulim <tomer10.katz@gmail.com>
2026-07-19 16:35:21 +0300

    add github support

--- lib/login/platform_login.dart
+++ lib/login/platform_login.dart
@@ -19,6 +19,12 @@ String googleSVG = '''
       </svg>
 ''';
 
+String githubSVG = '''
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg>
+''';
+
 class _PlatformLoginState extends State<PlatformLogin> {
   @override
   Widget build(BuildContext context) {
@@ -48,7 +54,7 @@ class _PlatformLoginState extends State<PlatformLogin> {
                   foregroundColor: Color(0xFFE3E3E3),
                   side: BorderSide(color: Color(0xFF8E918F), width: 1),
                   shape: StadiumBorder(),
-                  padding: EdgeInsets.only(left: 12, right: 20)
+                  padding: EdgeInsets.only(left: 12, right: 20),
                 ),
                 child: Row(
                   mainAxisSize: .min,
@@ -59,7 +65,7 @@ class _PlatformLoginState extends State<PlatformLogin> {
                       height: 30,
                       decoration: BoxDecoration(
                         // color: Colors.white,
-                        shape: BoxShape.circle
+                        shape: BoxShape.circle,
                       ),
                       padding: EdgeInsets.all(2),
                       margin: EdgeInsets.symmetric(vertical: 4),
@@ -71,7 +77,43 @@ class _PlatformLoginState extends State<PlatformLogin> {
                         fontSize: 14,
                         fontWeight: FontWeight.w500,
                         letterSpacing: 0.25,
-                        fontFamily: 'Roboto'
+                        fontFamily: 'Roboto',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color(0xFF131314),
+                  foregroundColor: Color(0xFFE3E3E3),
+                  side: BorderSide(color: Color(0xFF8E918F), width: 1),
+                  shape: StadiumBorder(),
+                  padding: EdgeInsets.only(left: 12, right: 20),
+                ),
+                child: Row(
+                  mainAxisSize: .min,
+                  spacing: 8,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      padding: EdgeInsets.all(2),
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      child: SvgPicture.string(githubSVG),
+                    ),
+                    Text(
+                      'Continue with github',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.25,
+                        fontFamily: 'Roboto',
                       ),
                     ),
                   ],
