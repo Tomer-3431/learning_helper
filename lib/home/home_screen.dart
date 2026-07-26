de8457e0303895509130d7ecb3e4b1ecc2479677
Hatulim <tomer10.katz@gmail.com>
2026-07-26 14:15:55 +0300

    move sidebar to new file

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learning_helper/constants/color_constants.dart';
 import 'package:learning_helper/constants/ui_constants.dart';
-import 'package:learning_helper/db/db_constants.dart' show anonyName;
-import 'package:learning_helper/db/global.dart';
+import 'package:learning_helper/home/sidebar.dart';
 import 'package:learning_helper/login/signin_utils.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -22,63 +20,11 @@ class _HomeScreenState extends State<HomeScreen> {
           logoutAction(context)
         ],
       ),
-      drawer: Drawer(
-        elevation: 8,
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            spacing: 10,
-            children: [
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Hello ',
-                      style: TextTheme.of(context).headlineLarge,
-                    ),
-                    TextSpan(
-                      text: currentUser?.name ?? anonyName,
-                      style: TextTheme.of(
-                        context,
-                      ).headlineLarge?.copyWith(color: primaryColor),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 5),
-              _navRow(icon: Icons.quiz, label: 'Quiz Select', destination: Container()),
-            ],
-          ),
-        ),
-      ),
+      drawer: Sidebar(),
       body: Padding(
         padding: EdgeInsets.all(32.0),
         child: Center(child: Text('Home Screen', style: h1)),
       ),
     );
   }
-
-  Widget _navRow({
-    required IconData icon,
-    required String label,
-    required Widget destination,
-  }) => GestureDetector(
-    onTap: () =>
-        Navigator.push(context, MaterialPageRoute(builder: (_) => destination)),
-    child: Row(
-      spacing: 5,
-      children: [
-        Icon(icon),
-        Text(label),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: .end,
-            children: [Icon(Icons.arrow_forward_ios)],
-          ),
-        ),
-      ],
-    ),
-  );
 }
---
+++ lib/home/sidebar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:learning_helper/constants/color_constants.dart';
+import 'package:learning_helper/db/db_constants.dart';
+import 'package:learning_helper/db/global.dart';
+
+class Sidebar extends StatefulWidget {
+  const Sidebar({super.key});
+
+  @override
+  State<Sidebar> createState() => _SidebarState();
+}
+
+class _SidebarState extends State<Sidebar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 8,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          spacing: 10,
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Hello ',
+                    style: TextTheme.of(context).headlineLarge,
+                  ),
+                  TextSpan(
+                    text: currentUser?.name ?? anonyName,
+                    style: TextTheme.of(
+                      context,
+                    ).headlineLarge?.copyWith(color: primaryColor),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            _navRow(
+              icon: Icons.quiz,
+              label: 'Quiz Select',
+              destination: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _navRow({
+    required IconData icon,
+    required String label,
+    required Widget destination,
+  }) => GestureDetector(
+    onTap: () =>
+        Navigator.push(context, MaterialPageRoute(builder: (_) => destination)),
+    child: Row(
+      spacing: 5,
+      children: [
+        Icon(icon),
+        Text(label),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: .end,
+            children: [Icon(Icons.arrow_forward_ios)],
+          ),
+        ),
+      ],
+    ),
+  );
+}
